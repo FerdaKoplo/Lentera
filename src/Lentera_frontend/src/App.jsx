f4f84ff4b23c093e19f931f250b088015b4af7d5
FerdaKoplo <sahmuraivan43@gmail.com>
2025-07-09 00:03:29 +0700

    add tailwind

--- src/Lentera_frontend/src/App.jsx
+++ src/Lentera_frontend/src/App.jsx
@@ -1,31 +1,13 @@
 import { useState } from 'react';
-import { Lentera_backend } from 'declarations/Lentera_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    Lentera_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
+ 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <h1 className='text-red-500'>test</h1>
     </main>
   );
 }
+
 
 export default App;
