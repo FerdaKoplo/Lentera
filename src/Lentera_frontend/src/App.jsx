fdc3637dce9384934b1bdc6f9a66aa31bb070ee7
FerdaKoplo <sahmuraivan43@gmail.com>
2025-07-14 21:45:48 +0700

    progress ui editor article

--- src/Lentera_frontend/src/App.jsx
+++ src/Lentera_frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import SideBarPrincipal from './components/principal/sidebar-principal';
 
 function App() {
  
   return (
     <main>
+      <SideBarPrincipal></SideBarPrincipal>
       <h1 className='text-red-500'>test</h1>
     </main>
   );
---
+++ src/Lentera_frontend/src/pages/principal/articles/create-article.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CreateArticle = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default CreateArticle
